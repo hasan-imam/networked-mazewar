d1228dd1d5bcf75c1c4d0cbb6ffb8f3af3e398c6
Hasan Imam <hasan.imam.w@gmail.com>
2013-04-15 11:10:35 -0400

    add run scripts for server and game client

---
+++ client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Arguments:
+# $1 = server's IP
+# $2 = server's listening port
+
+java Mazewar $1 $2
---
+++ server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Arguments:
+# $1 = server's listening port. Make sure that it's not being used by some other program
+
+java MazeServer $1
+
